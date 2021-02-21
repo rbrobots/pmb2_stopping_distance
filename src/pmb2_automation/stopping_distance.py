a21325b4156e56498330c864e267d1f0199faef4
Ranvir <rbrobots20@gmail.com>
2021-02-21 21:47:44 +0000

    update function syntax for twist msg

--- src/pmb2_automation/stopping_distance.py
+++ src/pmb2_automation/stopping_distance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Year: 2021
 # Author: Ranvir Bhogal
+# Description: Stopping distance simulation with TIAgo robot
 
 import math
 import rospy
@@ -10,30 +11,34 @@ class Automation():
     def __init__(self):
         print("Initialised")#insert publishers and subscribers below
         self._pub_vel = rospy.Publisher('key_vel',Twist, queue_size=10) # change pub name to my own pub
+        self._sub_odom = rospy.Subscriber("/mobile_base_controller/odom",Twist, self.odom_callback)#does this need to be TWIST msg?
+        self._distance = 0#change to relevant distance
+        self.twist = Twist()
 
-#        self.rospy.Subscriber("robot_pose") #NEED TO GET FEEDBACK OF ROBOT POSITION - TO UPDATE * 
-# add velocity boundaries
-
+    def odom_callback(self,output):
+        print("..in odom_callback function..")
+        print("..incoming msg ="+output.data)
 
     def setPMB2Orientation():#set default orientation in corridoor
         print("in setPMB2Orientation")
 
-    def sendVelocity(self):#make parameters dynamic later
+    def setVelocity(self, linear, angular):#make parameters dynamic later
         print("In sendVelocity() function")
+        self.twist.linear.x = linear
+        self.twist.angular.z = angular
 
     def run(self):
         self._linear=0 #initialise var to zero
         self._angular=0
         self._hz = rospy.Rate(1) #MIGHT NEED TO CHANGE THIS
 
-
     def publish(self):
-        twist=Twist()
-        twist.linear.x = 0.5
-        twist.angular.z = 1
-        while True:
-            self._pub_vel.publish(twist)
+        self.setVelocity(0.5,0.0)
+
+        while True:#infinite loop - change this
+            self._pub_vel.publish(self.twist)
         print("PUBLISHED")
+        #if reached distance x, send zero velocity
 
 
 if __name__ == '__main__':
