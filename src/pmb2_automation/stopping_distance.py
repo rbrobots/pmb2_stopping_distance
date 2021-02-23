1e0d33bfdd931e40e082cd9b59ec7d8d0831f0d0
Ranvir <rbrobots20@gmail.com>
2021-02-23 19:31:10 +0000

    automation script works. Need to add more scenarios, lift speed restriction, add weights, change flooring etc.

--- src/pmb2_automation/stopping_distance.py
+++ src/pmb2_automation/stopping_distance.py
@@ -6,6 +6,7 @@
 import math
 import rospy
 import time
+import numpy as np
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose
 from gazebo_msgs.msg import ModelState
@@ -16,12 +17,14 @@ class Automation():
     def __init__(self):
         print("Initialised")#insert publishers and subscribers below
         self._pub_model_state = rospy.Publisher('/gazebo/set_model_state',ModelState,queue_size=10)
-        self._pub_vel = rospy.Publisher('key_vel',Twist, queue_size=10) # change pub name to my own pub
+        #self._pub_vel = rospy.Publisher('key_vel',Twist, queue_size=10) # change pub name to my own pub
+        self._pub_vel = rospy.Publisher('/mobile_base_controller/cmd_vel',Twist, queue_size=10)
+
         self._pub_data = rospy.Publisher('stop_dist_data',Data,queue_size=10)
         self._sub_odom = rospy.Subscriber('/mobile_base_controller/odom',Odometry, self.odom_callback)
 
         self.model_state = ModelState()#for setting model state at each iteration of test scenario
-        self.twist = Twist()#target velocity
+        self.target_velocity = Twist()#target velocity
         self.robot_odometry = Odometry()#robot odometry values
         self.timer1 =0
         self.timer2 =0
@@ -37,11 +40,11 @@ class Automation():
 
         #self.data_recorder=DataRecorder()
 
-        #self.initModelState()#set initial pose of robot
+        #self.pub_model_state()#set initial pose of robot
 
     def reset(self):#CHECK THIS
         self.model_state = ModelState()#for setting model state at each iteration of test scenario
-        self.twist = Twist()#target velocity
+        self.target_velocity = Twist()#target velocity
         self.robot_odometry = Odometry()#robot odometry values
         self.timer1 =0
         self.timer2 =0
@@ -60,8 +63,8 @@ class Automation():
         self.robot_odometry.twist.twist.linear.x = robot.twist.twist.linear.x
         self.robot_odometry.twist.twist.angular.z = robot.twist.twist.angular.z
 
-    def setModelState(self,p_x,p_y,p_z,o_x,o_y,o_z,o_w):# add rest of arguments
-        self.model_state.model_name = "pmb2"
+    def set_model_state(self,p_x,p_y,p_z,o_x,o_y,o_z,o_w):# add rest of arguments
+        self.model_state.model_name = "pmb2"#this should be dynamic later
         self.model_state.pose.position.x = p_x
         self.model_state.pose.position.y = p_y
         self.model_state.pose.position.z = p_z
@@ -69,6 +72,7 @@ class Automation():
         self.model_state.pose.orientation.y= o_y
         self.model_state.pose.orientation.z= o_z#perp to wall in corridoor
         self.model_state.pose.orientation.w =o_w
+        #all twist values hardcoded to zero for this case
         self.model_state.twist.linear.x=0.0
         self.model_state.twist.linear.y=0.0
         self.model_state.twist.linear.z=0.0
@@ -77,40 +81,74 @@ class Automation():
         self.model_state.twist.angular.z=0.0
         self.model_state.reference_frame = "world"
 
-    def initModelState(self):#set default pmb2 pose in corridoor
+    def pub_model_state(self):#set default pmb2 pose in corridoor
         time.sleep(2)
-        self.setModelState(0.0,0.0,0.0,0.0,0.0,1.57,1.0)
+        self.set_model_state(self.model_state.pose.position.x,self.model_state.pose.position.y,self.model_state.pose.position.z,
+            self.model_state.pose.orientation.x,self.model_state.pose.orientation.y,self.model_state.pose.orientation.z,
+            self.model_state.pose.orientation.w)
+        #self.set_model_state(0.0,0.0,0.0,0.0,0.0,1.57,1.0)
         self._pub_model_state.publish(self.model_state)
         time.sleep(2)
-        print("model state initialised ",self.model_state.pose.orientation.z)
+        #print("model state initialised with pmb2 orientation.z=",self.model_state.pose.orientation.z)
 
-    def setVelocity(self, linear, angular):#make parameters dynamic later
+    def set_target_velocity(self, linear, angular):#this is just for storage. Not published yet
         #print("In sendVelocity() function")
-        self.twist.linear.x = linear
-        self.twist.angular.z = angular
+        self.target_velocity.linear.x = linear
+        self.target_velocity.angular.z = angular
 
-    def run(self):
-        print("run")
-        #self._hz = rospy.Rate(1) #MIGHT NEED TO CHANGE THIS
+    def pub_target_velocity(self):
+        self._pub_vel.publish(self.target_velocity)
 
     def calc_stop_dist(self,n):
         if(n==1):#initial zero velocity command sent
             self.stop_dist_1.position.x = self.robot_odometry.pose.pose.position.x
             self.stop_dist_1.orientation.z = self.robot_odometry.pose.pose.orientation.z
-            print("stop_dist_1 x=",self.stop_dist_1.position.x," stop_dist_1 z=",self.stop_dist_1.orientation.z)
+            #print("stop_dist_1 x=",self.stop_dist_1.position.x," stop_dist_1 z=",self.stop_dist_1.orientation.z)
             self.sd1 = True
         elif(n==2):#physical robot stops with zero velocity
             self.stop_dist_2.position.x = self.robot_odometry.pose.pose.position.x
             self.stop_dist_2.orientation.z = self.robot_odometry.pose.pose.orientation.z
-            print("stop_dist_2 x=",self.stop_dist_2.position.x," stop_dist_2 z=",self.stop_dist_2.orientation.z)
+            #print("stop_dist_2 x=",self.stop_dist_2.position.x," stop_dist_2 z=",self.stop_dist_2.orientation.z)
             self.sd2 = True
         elif(n==3):#calculate absolute? difference between 2 and 1;
             self.stop_dist_actual.position.x = abs(self.stop_dist_2.position.x - self.stop_dist_1.position.x)
             self.stop_dist_actual.orientation.z = abs(self.stop_dist_2.orientation.z - self.stop_dist_1.orientation.z)
-            print("stop_act_x=",self.stop_dist_actual.position.x," stop_act_z=",self.stop_dist_actual.orientation.z)
+            #print("stop_act_x=",self.stop_dist_actual.position.x," stop_act_z=",self.stop_dist_actual.orientation.z)
 
-    def testScript():#this script will iterate through a number of test scenarios
-        self.setVelocity(0.1,0.0) # 2d for loop iterate all speeds
+
+    def calc_elapsed_time(self,n):
+        if(n==1):
+            self.timer1 = time.time()
+        elif(n==2):
+            self.timer2 = time.time()
+        elif(n==3):
+            self.time_elapsed = self.timer2-self.timer1
+
+    def round_decimals_up(self, number, decimals):#from kodify.net
+        if not isinstance(decimals, int):
+            raise TypeError("decimal places must be an integer")
+        elif decimals < 0:
+            raise ValueError("decimal places has to be 0 or more")
+        elif decimals == 0:
+            return math.ceil(number)
+
+        factor = 10 ** decimals
+        return math.ceil(number * factor) / factor
+
+    def execute_test_scripts(self):#this script will iterate through a number of test scenarios
+        
+        #MOVING FORWARD IN X AXIS
+        for x in np.arange(0.10, 1.10, 0.10):#iterate all linear speeds (0.1-1.0) for x within speed restrictions.
+            print("..running test case cmd vel x=",x,"..")
+            aut.execute_single_test_script(x,0.0)
+            print("..test case cmd vel x=",x," completed..")
+
+        #REVERSING IN X AXIS?
+
+        #ANGULAR SPEED IN Z AXIS?
+
+
+        #self.set_target_velocity(0.1,0.0) # 2d for loop iterate all speeds
         #all forward cases
         ##linear
         ##angular
@@ -120,8 +158,48 @@ class Automation():
         ##angular
         ##both
 
+    def execute_single_test_script(self, x, z):
+        self.reset()
+        #self.set_model_state(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+        self.set_model_state(0.0,0.0,0.0,0.0,0.0,1.5708,1.0)#initial model state always the same
+        self.pub_model_state()
+        #initialisations done
+
+        self.set_target_velocity(x,z)
+        self.pub_target_velocity()
+
+        time.sleep(2)
+
+        while True:
+            if(round(self.robot_odometry.twist.twist.linear.x,2) >= round(self.target_velocity.linear.x,1)):#if robot has reached target velocity
+                #print("(IN 1) 2 dp odom=",round(self.robot_odometry.twist.twist.linear.x,2))
+                self.set_target_velocity(0.0,0.0)
+                self.pub_target_velocity()
+                if(self.sd1==False):
+                    self.calc_stop_dist(1)#starting calculating stop distance
+                self.calc_elapsed_time(1)#start calculating time to real stop
+                self.movement=False
+            else:
+                self.pub_target_velocity()#if not at target velocity, send velocity command to robot for this test case
+                #print("(IN 2) 2 dp odom=",round(self.robot_odometry.twist.twist.linear.x,2))
+                print("current=",round(self.robot_odometry.twist.twist.linear.x,2),
+                    ", target=",round(self.target_velocity.linear.x,1))
+
+
+            if(self.robot_odometry.twist.twist.linear.x<=0 and self.movement==False):#if odometry shows robot has stopped
+                if(self.sd2==False):
+                    self.calc_stop_dist(2)#stop calculating stop distance
+                self.calc_elapsed_time(2)#stop caclulating stop distance time
+                self.calc_elapsed_time(3)#calculate time taken to stop robot after receiving zero velocity
+                self.calc_stop_dist(3)#calculate actual stop distance
+                #print("(IN 3) 2 dp odom=",round(self.robot_odometry.twist.twist.linear.x,2))
+                break
+        self.pub_data()#publish collected data to topic for results analysis
+
+
+
     def pub_data(self):#this function publishes data for output
-        self.data.desired_velocity.linear.x = self.twist.linear.x
+        self.data.desired_velocity.linear.x = self.target_velocity.linear.x
         self.data.initial_pose.position.x=0#TO ADD
         self.data.initial_pose.position.z=0#TO ADD
         self.data.final_pose.position.x=0#TO ADD
@@ -135,11 +213,12 @@ class Automation():
         time.sleep(2)
 
     def publish(self):
-        self.setVelocity(0.7,0.0)
+        #self.set_target_velocity(5,0.0)
         while True:#while robot local velocity <= commanded velocity
-            if(round(self.robot_odometry.twist.twist.linear.x,2) >= self.twist.linear.x):#we only check linear velocity
+
+            if(round(self.robot_odometry.twist.twist.linear.x,2) >= self.target_velocity.linear.x):#we only check linear velocity
                 #print("WE HIT DESIRED VELOCITY")
-                self.setVelocity(0.0,0.0)#is this needed because the robot stopped receiving velocity commands?
+                self.set_target_velocity(0.0,0.0)#this doesn't do anything
                 
                 if(self.sd1==False):#calc initial stop dist
                 #get pose from robot
@@ -149,7 +228,9 @@ class Automation():
                 #print("Elapsed Time:")
                 self.movement=False#this might slow down readings
             else:
-                self._pub_vel.publish(self.twist)
+                self._pub_vel.publish(self.target_velocity)
+
+
             if(self.robot_odometry.twist.twist.linear.x<=0 and self.movement==False):
                 print("ROBOT HAS STOPPED 1")
 
@@ -180,12 +261,16 @@ if __name__ == '__main__':
         aut = Automation()#instantiate class
 
         rate = rospy.Rate(1)#publish at 1 Hz/s -- should I change the rate?
-        time.sleep(2)
-        aut.initModelState()
-        time.sleep(2)
+        #time.sleep(2)
+        #aut.pub_model_state()
+        #time.sleep(2)
 
-        aut.run()
-        aut.publish()
+        #aut.publish()
+
+        #aut.execute_test_scripts()
+        #aut.execute_single_test_script(0.9,0.0)
+
+        aut.execute_test_scripts()
 
         #while not rospy.is_shutdown():
             #rate.sleep()
